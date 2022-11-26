b624ad7fb6f387a0964e3311582b84ef4dd7425e
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-11-26 11:25:35 +0530

    updated project

--- About.py
+++ About.py
@@ -4,11 +4,13 @@ def show_about_page():
 
 
     st.title("About")
-    st.write("""### This is a simple app to predict the salary of a software engineer based on the following parameters:
-    - Country
-    - Education Level
-    - Years of Experience
-    """)
+    st.header("""This is a simple app to predict the salary of a software engineer based on the following parameters""")
+    st.subheader("""Country""")
+    st.subheader("""Years of Experience""")
+    st.subheader("""Education Level""")
+
+    
+    
     st.write("""### The data used for this project is from the Stack Overflow Developer Survey 2020. The data can be found [here](https://survey.stackoverflow.co/2022/ )""")
     
     st.write(
